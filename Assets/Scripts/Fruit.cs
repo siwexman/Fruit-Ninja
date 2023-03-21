13fe1b791b3b7a03083c87959f641955c1110508
siwexman <karol.straczek@gmail.com>
2023-03-21 20:13:57 +0100

    Initial commit

---
+++ Assets/Scripts/Fruit.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruit : MonoBehaviour
+{
+    public GameObject slicedFruitPrefab;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateSlicedFruit();
+        }
+    }
+    public void CreateSlicedFruit()
+    {
+        GameObject inst = (GameObject)Instantiate(slicedFruitPrefab,transform.position,transform.rotation);
+
+
+        Rigidbody[] rbsOnSliced = inst.GetComponentsInChildren<Rigidbody>();
+        
+        foreach (Rigidbody r in rbsOnSliced)
+        {
+            r.transform.rotation = Random.rotation;
+            r.AddExplosionForce(Random.Range(500, 1000), transform.position, 5f);
+        }
+        Destroy(gameObject);
+    }
+}
