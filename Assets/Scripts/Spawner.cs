0b960941841c9926b4710c3877e1c7ab5dfe1c25
siwexman <karol.straczek@gmail.com>
2023-03-22 13:43:08 +0100

    2

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject fruitToSpawn;
+    public Transform[] spawnPlaces;
+    public float minWait = .3f;
+    public float maxWait = 1f;
+    public float minForce = 12;
+    public float maxForce = 17;
+
+
+
+    void Start()
+    {
+        StartCoroutine(SpawnFruits());
+    }
+
+    private IEnumerator SpawnFruits()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minWait, maxWait));
+
+            Transform t = spawnPlaces[Random.Range(0, spawnPlaces.Length)];
+
+            GameObject fruit = Instantiate(fruitToSpawn, t.position, t.rotation);
+
+            fruit.GetComponent<Rigidbody2D>().AddForce(t.transform.up * Random.Range(minForce,maxForce), ForceMode2D.Impulse);
+
+            Debug.Log("Fruit get spawned");
+
+            Destroy(fruit, 5f);
+        }
+    }
+
+}
